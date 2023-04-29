901078b1c7453d7305db92d3709386979cf14043
GastarTestProjects <53343040+GastarTestProjects@users.noreply.github.com>
2023-04-30 01:48:03 +0400

    Rooms generation

---
+++ Assets/Scripts/Map/RoomConstructionConfig.cs
@@ -0,0 +1,19 @@
+﻿using Common;
+using UnityEngine;
+
+namespace Map
+{
+    public struct RoomConstructionConfig
+    {
+        public readonly WallDirection[] doors;
+        public readonly Vector3 position;
+        public readonly Transform parent;
+        
+        public RoomConstructionConfig(WallDirection[] doors, Vector3 position, Transform parent)
+        {
+            this.doors = doors;
+            this.position = position;
+            this.parent = parent;
+        }
+    }
+}
---
+++ Assets/Scripts/Map/RoomFactory.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using Common;
+using UnityEngine;
+
+namespace Map
+{
+    public class RoomFactory: MonoBehaviour
+    {
+        [Header("Room parts prefabs")]
+        [SerializeField] private GameObject ground;
+        [SerializeField] private GameObject wallTop;
+        [SerializeField] private GameObject wallTopDoor;
+        [SerializeField] private GameObject wallRight;
+        [SerializeField] private GameObject wallRightDoor;
+        [SerializeField] private GameObject wallBottom;
+        [SerializeField] private GameObject wallBottomDoor;
+        [SerializeField] private GameObject wallLeft;
+        [SerializeField] private GameObject wallLeftDoor;
+            
+        public GameObject CreateRoom(RoomConstructionConfig config)
+        {
+            var room = new GameObject(GetRoomName(config));
+            room.transform.SetParent(config.parent);
+            room.transform.position = config.position;
+            
+            CreateGround(config, room);
+            CreateAllWalls(config, room);
+            
+            return room;
+        }
+        
+        private void CreateGround(RoomConstructionConfig config, GameObject room)
+        {
+            var ground = Instantiate(this.ground, config.position, Quaternion.identity, room.transform);
+            // TODO: ground variations
+        }
+        
+        private void CreateAllWalls(RoomConstructionConfig config, GameObject room)
+        {
+            if (config.doors.Contains(WallDirection.Top))
+                Instantiate(wallTopDoor, config.position, Quaternion.identity, room.transform);
+            else
+                Instantiate(wallTop, config.position, Quaternion.identity, room.transform);
+            
+            if (config.doors.Contains(WallDirection.Right))
+                Instantiate(wallRightDoor, config.position, Quaternion.identity, room.transform);
+            else
+                Instantiate(wallRight, config.position, Quaternion.identity, room.transform);
+            
+            if (config.doors.Contains(WallDirection.Bottom))
+                Instantiate(wallBottomDoor, config.position, Quaternion.identity, room.transform);
+            else
+                Instantiate(wallBottom, config.position, Quaternion.identity, room.transform);
+            
+            if (config.doors.Contains(WallDirection.Left))
+                Instantiate(wallLeftDoor, config.position, Quaternion.identity, room.transform);
+            else
+                Instantiate(wallLeft, config.position, Quaternion.identity, room.transform);
+        }
+        
+        private string GetRoomName(RoomConstructionConfig config)
+        {
+            var name = "Room_";
+            foreach (var door in config.doors)
+            {
+                name += door.ToString();
+            }
+
+            return name;
+        }
+        
+    }
+}
