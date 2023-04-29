ca60bc3b065c188ca2e2723faf0cf9645e83cfed
Shams-C <127062585+Shams-C@users.noreply.github.com>
2023-04-30 01:00:39 +0400

    Added a weapon to the character

---
+++ Assets/Scripts/Common/Player/Gun.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Common.Player
+{
+    public class Gun : MonoBehaviour
+    {
+        
+        private void Update()
+        {
+        }
+    }
+}
--- Assets/Scripts/Common/Player/Player.cs
+++ Assets/Scripts/Common/Player/Player.cs
@@ -1,16 +1,20 @@
-using System;
-using TMPro;
 using UnityEngine;
 
 namespace Common.Player
 {
-    public class PlayerMovement : MonoBehaviour
+    public class Player : MonoBehaviour
     {
         private Rigidbody2D _rigidbody2D;
         private Animator _animator;
         private Vector2 _movementDirection;
-        [SerializeField] private float speedPlayerMovement = 5f;
         private static readonly int AnimPlayerMove = Animator.StringToHash("AminPlayerMove");
+        
+        [SerializeField] private new Camera camera;
+        [SerializeField] private float gunDistance = 0.6f;
+        [SerializeField] private Transform gunTransform;
+        [SerializeField] private float speedPlayerMovement = 5f;
+        
+        
 
         private void Start()
         {
@@ -22,6 +26,7 @@ namespace Common.Player
         {
             ProcessInput();
             AnimatePLayer();
+            RotateGunAroundPlayer();
         }
 
         private void FixedUpdate()
@@ -67,6 +72,26 @@ namespace Common.Player
 
            _animator.SetInteger(AnimPlayerMove, direction);
        }
-        
+       
+       private void RotateGunAroundPlayer()
+       {
+           var position = transform.position;
+           var mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
+           var direction = (mousePosition - position).normalized;
+
+           var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+           gunTransform.rotation = Quaternion.Euler(0, 0, angle);
+           
+           var gunPosition = position + Quaternion.Euler(0.4f, 0, angle) * Vector3.right * gunDistance;
+           gunPosition.z = -1;
+           gunPosition.y -= 0.2f;
+           gunTransform.position = gunPosition;
+           FlipGun(angle);
+       }
+       
+       private void FlipGun(float angle)
+       {
+           gunTransform.localScale = angle is > 90 or < -90 ? new Vector3(1, -1, 1) : new Vector3(1, 1, 1);
+       }
     }
 }
