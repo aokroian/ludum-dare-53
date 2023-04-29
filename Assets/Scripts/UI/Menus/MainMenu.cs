a1fe2a0bd9e4f74d6f0481e21d7b56b6aca2ab3b
GastarTestProjects <53343040+GastarTestProjects@users.noreply.github.com>
2023-04-29 21:46:11 +0400

    Add scene switching

---
+++ Assets/Scripts/UI/Menus/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Scene;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Menus
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField] private Button startBtn;
+        [SerializeField] private Button exitBtn;
+
+        private void Awake()
+        {
+            startBtn.onClick.AddListener(StartGame);
+            exitBtn.onClick.AddListener(ExitGame);
+        }
+        
+        private void StartGame()
+        {
+            SceneController.Instance.LoadGameScene();
+        }
+        
+        private void ExitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
