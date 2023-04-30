a2e4d53598c8ab99d2550a6a18c783d78da570fc
Shams-C <127062585+Shams-C@users.noreply.github.com>
2023-04-30 22:46:10 +0400

    inplemented spawn particle

--- Assets/Scripts/Actors/Combat/Bullet.cs
+++ Assets/Scripts/Actors/Combat/Bullet.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Actors.Combat
@@ -5,10 +6,22 @@ namespace Actors.Combat
     public class Bullet : MonoBehaviour
     {
         [SerializeField] private float speedBullet;
-        
+        [SerializeField] private GameObject particle;
+
         void Update()
         {
             transform.Translate(Vector2.right * (speedBullet * Time.deltaTime));
         }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            var position = transform.position;
+            var hit = Physics2D.Raycast(position, other.transform.position - position);
+            Vector3 normal = hit.normal;
+            var spawned = Instantiate(particle, hit.point, Quaternion.identity);
+            spawned.transform.forward = normal;
+            Destroy(gameObject);
+        }
     }
 }
+
