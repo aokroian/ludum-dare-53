2e1be21062cf36bdc287884c2a09123a6274cd1e
Shams-C <127062585+Shams-C@users.noreply.github.com>
2023-05-01 02:18:51 +0400

    weapon prefabs

--- Assets/Scripts/Actors/ActorGunSystem.cs
+++ Assets/Scripts/Actors/ActorGunSystem.cs
@@ -1,18 +1,19 @@
 using Actors.Combat;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Actors
 {
     public class ActorGunSystem : ActorSystem
     {
         [SerializeField] private float gunDistance = 0.6f;
-        [SerializeField] private Gun gun;
+        [SerializeField] private Gun gun1;
 
         private void Update()
         {
             RotateGunAroundPlayer();
             if (ActorInput.Fire)
-                gun.Fire();
+                gun1.Fire();
         }
 
 
@@ -23,13 +24,13 @@ namespace Actors
             var direction = (mousePosition - position).normalized;
 
             var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            gun.transform.rotation = Quaternion.Euler(0, 0, angle);
+            gun1.transform.rotation = Quaternion.Euler(0, 0, angle);
 
             var gunPosition = position + Quaternion.Euler(0.4f, 0, angle) * Vector3.right * gunDistance;
             gunPosition.z = -1;
             gunPosition.y -= 0.2f;
-            gun.transform.position = gunPosition;
-            gun.FlipSprite(angle);
+            gun1.transform.position = gunPosition;
+            gun1.FlipSprite(angle);
         }
     }
 }
