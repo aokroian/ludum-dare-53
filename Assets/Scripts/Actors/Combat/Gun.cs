0091bcbf187af194adc8fa33ff8db7e87fdd6642
Shams-C <127062585+Shams-C@users.noreply.github.com>
2023-04-30 18:22:11 +0400

    implemented bullets dispersion

--- Assets/Scripts/Actors/Combat/Gun.cs
+++ Assets/Scripts/Actors/Combat/Gun.cs
@@ -9,7 +9,8 @@ namespace Actors.Combat
         [SerializeField] private Transform muzzle;
         [SerializeField][Range(0.01f,30f)] private float shootRate;
         [SerializeField] private GameObject bullet;
-
+        [SerializeField][Range(1,7)] private int quantityShot = 3;
+        [SerializeField][Range(0, 360)] private float totalAngle = 30f;
 
         private float _shootRateTimer;
         
@@ -22,10 +23,19 @@ namespace Actors.Combat
         {
             if (_shootRateTimer <= 0)
             {
-                Instantiate(bullet, muzzle.position, transform.rotation);
+                float angleBetweenBullets = 0;
+                if (quantityShot > 1)
+                {
+                    angleBetweenBullets = totalAngle / (quantityShot - 1);
+                }
+
+                var initialAngle = -(totalAngle / 2);
+                for (var i = 0; i < quantityShot; i++)
+                {
+                    var bulletRotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z + initialAngle + (i * angleBetweenBullets));
+                    Instantiate(bullet, muzzle.position, bulletRotation);
+                }
                 _shootRateTimer = 1 / shootRate;
-                
-                
             }
         }
     }
