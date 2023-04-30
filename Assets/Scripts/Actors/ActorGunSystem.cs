0680d750e056229bd68f31cf8aca09b002b85a0c
Shams-C <127062585+Shams-C@users.noreply.github.com>
2023-05-01 02:34:08 +0400

    refactoring code

--- Assets/Scripts/Actors/ActorGunSystem.cs
+++ Assets/Scripts/Actors/ActorGunSystem.cs
@@ -7,13 +7,13 @@ namespace Actors
     public class ActorGunSystem : ActorSystem
     {
         [SerializeField] private float gunDistance = 0.6f;
-        [SerializeField] private Gun gun1;
+        [SerializeField] private Gun gun;
 
         private void Update()
         {
             RotateGunAroundPlayer();
             if (ActorInput.Fire)
-                gun1.Fire();
+                gun.Fire();
         }
 
 
@@ -24,13 +24,13 @@ namespace Actors
             var direction = (mousePosition - position).normalized;
 
             var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            gun1.transform.rotation = Quaternion.Euler(0, 0, angle);
+            gun.transform.rotation = Quaternion.Euler(0, 0, angle);
 
             var gunPosition = position + Quaternion.Euler(0.4f, 0, angle) * Vector3.right * gunDistance;
             gunPosition.z = -1;
             gunPosition.y -= 0.2f;
-            gun1.transform.position = gunPosition;
-            gun1.FlipSprite(angle);
+            gun.transform.position = gunPosition;
+            gun.FlipSprite(angle);
         }
     }
 }
