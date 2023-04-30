8753db758618b398acd6f0f48e866844917c0c23
Shams-C <127062585+Shams-C@users.noreply.github.com>
2023-04-30 20:57:47 +0400

    implemented destroy particles and bullet

--- Assets/Scripts/Actors/Combat/Bullet.cs
+++ Assets/Scripts/Actors/Combat/Bullet.cs
@@ -5,16 +5,10 @@ namespace Actors.Combat
     public class Bullet : MonoBehaviour
     {
         [SerializeField] private float speedBullet;
-        private float _timer;
-        private const float TimeDestroy = 2f;
         
         void Update()
         {
-            _timer += Time.deltaTime;
             transform.Translate(Vector2.right * (speedBullet * Time.deltaTime));
-            if (!(_timer >= TimeDestroy)) return;
-            Destroy(gameObject);
-            _timer = 0;
         }
     }
 }
