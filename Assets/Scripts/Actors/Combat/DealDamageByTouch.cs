e59b31458bc6e535036b27ff494d78473769b587
GodPlaysDice <49920714+GodDoesPlayDice@users.noreply.github.com>
2023-05-02 00:10:58 +0400

    touch enemies damage

--- Assets/Scripts/Actors/Combat/DealDamageByTouch.cs
+++ Assets/Scripts/Actors/Combat/DealDamageByTouch.cs
@@ -1,30 +1,57 @@
+using System;
+using DG.Tweening;
 using UnityEngine;
 
 namespace Actors.Combat
 {
     public class DealDamageByTouch : MonoBehaviour
     {
-        [SerializeField] private Collider2D damageCollider;
-        [SerializeField] private float cooldownBetweenDamage = 0.4f;
-        [SerializeField] private int damage = 10;
+        [SerializeField] private float cooldownBetweenDamage = 2f;
+        [SerializeField] private int damage = 1;
 
         public Transform ownerActor;
+        private string OwnerActorTag => ownerActor.tag;
 
-        private float _previousDamageTime;
+        private float _damageTimer;
+
+        private void Update()
+        {
+            _damageTimer += Time.time;
+        }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
-            if (other.transform.IsChildOf(ownerActor) ||
-                !other.CompareTag("Enemy") || !other.CompareTag("Player"))
-            {
-                return;
-            }
+            TryToDealDamage(other);
+        }
 
-            var actorHealth = other.GetComponentInChildren<ActorHealth>();
-            if (actorHealth == null || Time.time - _previousDamageTime <= cooldownBetweenDamage)
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            TryToDealDamage(other);
+        }
+
+        private void TryToDealDamage(Collider2D other)
+        {
+            if (other.transform.IsChildOf(ownerActor) || other.CompareTag(OwnerActorTag))
                 return;
-            actorHealth.TakeDamage(damage);
-            _previousDamageTime = Time.time;
+
+            if (_damageTimer > cooldownBetweenDamage)
+            {
+                var actorHealth = other.GetComponentInChildren<ActorHealth>();
+                if (actorHealth == null)
+                    return;
+                actorHealth.TakeDamage(damage);
+                Debug.Log("damage");
+                PushVictim(other.transform);
+                _damageTimer = 0;
+            }
+        }
+
+        private void PushVictim(Transform victim)
+        {
+            var victimPos = victim.position;
+            var dir = victimPos - transform.position;
+            var targetPos = victimPos + dir * .8f;
+            victim.DOMove(targetPos, 0.25f).SetEase(Ease.OutElastic);
         }
     }
 }
